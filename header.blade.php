b64bbdf10f1d718e8cd841a9eb4b0c3f40107892
yamnat <63818053+yamnat@users.noreply.github.com>
2021-06-04 16:31:31 +0530

    Add files via upload

---
+++ header.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">E-comm</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="#">Home</a></li>
+      <li><a href="#">Orders</a></li>
+      <li><a href="#">contact us</a></li>
+    </ul>
+    <form class="navbar-form navbar-left" action="#">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-cart"></span> Add to Cart</a></li>
+    </ul>
+  </div>
+</nav>
